85c17d2bfc5303d7bd889915fe0efb37a0f2e215
LiuXYh <104259882+LiuXYh@users.noreply.github.com>
2022-04-28 11:22:58 +0800

    Add files via upload

---
+++ R/UpSetVP-package.R
@@ -0,0 +1,17 @@
+#' Visualization of Variation Partitioning and Hierarchical Partitioning
+#'
+#' UpSetVP using UpSet matrix layout to visualize the unique, common and individual contribution of each predictor (or matrix) towards explained variation on canonical analysis.
+#'
+#' @docType package
+#' @name UpSetVP-package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom grDevices colorRampPalette
+#' @importFrom vegan vegdist
+#' @importFrom rdacca.hp rdacca.hp
+#' @import ggplot2
+#' @import patchwork
+## usethis namespace: end
+NULL

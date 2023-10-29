2068719760629511b559a745bdcc8b3d32e46e5f
LiuXYh <104259882+LiuXYh@users.noreply.github.com>
2023-10-30 01:32:37 +0800

    Update upset_vp.R

--- R/upset_vp.R
+++ R/upset_vp.R
@@ -25,7 +25,7 @@
 #' @return \itemize{Returns a ggplot2.}
 #'
 #' @references Lai J., Zou Y., Zhang J., Peres-Neto P. (2022) Generalizing hierarchical and variation partitioning in multiple regression and canonical analyses using the rdacca.hp R package. Methods in Ecology and Evolution, 13:782-788.
-#' @references Liu Y., Yu X., Yu Y., et al. (2023) Application of "rdacca. hp" R package in ecological data analysis: case and progress. Chinese Journal of Plant Ecology, 27: 134-144.
+#' @references Liu Y., Yu X., Yu Y., et al. (2023) Application of "rdacca. hp" R package in ecological data analysis: case and progress. Chinese Journal of Plant Ecology, 27:134-144.
 #'
 #' @export
 #' @examples

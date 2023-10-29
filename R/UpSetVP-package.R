32200debc64e03415a9947707e2b104b21919b13
LiuXYh <104259882+LiuXYh@users.noreply.github.com>
2023-10-30 01:33:59 +0800

    Update UpSetVP-package.R

--- R/UpSetVP-package.R
+++ R/UpSetVP-package.R
@@ -3,7 +3,7 @@
 #' UpSetVP using UpSet matrix layout to visualize the unique, common and individual contribution of each predictor (or matrix) towards explained variation on canonical analysis.
 #'
 #' @references Lai J., Zou Y., Zhang J., Peres-Neto P. (2022) Generalizing hierarchical and variation partitioning in multiple regression and canonical analyses using the rdacca.hp R package. Methods in Ecology and Evolution, 13:782-788.
-#' @references Liu Y., Yu X., Yu Y., et al. (2023) Application of "rdacca. hp" R package in ecological data analysis: case and progress. Chinese Journal of Plant Ecology, 27: 134-144.
+#' @references Liu Y., Yu X., Yu Y., et al. (2023) Application of "rdacca. hp" R package in ecological data analysis: case and progress. Chinese Journal of Plant Ecology, 27:134-144.
 #'
 #' @docType package
 #' @name UpSetVP-package
